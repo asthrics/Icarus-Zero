0110986b8c1526345bf0cf6adae58b23b01bc2cf
asthrics <sebastianlercher@icloud.com>
2026-06-28 22:08:30 +0200

    Added VS Project

---
+++ Icarus-Zero Reactor/Icarus-Zero Reactor/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Icarus_Zero_Reactor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
